7284385bc250fd89452433ef6fb37411fb098827
Davi Thiesse <davithss@gmail.com>
2019-01-25 10:23:56 -0300

    remove redundant test

--- tictac/test/tictac_test.exs
+++ tictac/test/tictac_test.exs
@@ -4,10 +4,6 @@ defmodule TictacTest do
   alias Tictac.Game
 
   describe "starting play game with Supervisor" do
-    test "new game" do
-      assert {:ok, pid} = Tictac.new_game
-    end
-
     test "play at using game_pid" do
       {:ok, pid} = Tictac.new_game
       board = Game.new_board
